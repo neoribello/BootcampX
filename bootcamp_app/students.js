913347bf352b0c5f6bb0423144129aee7a9361e8
Neil Oribello <neil.oribello@gmail.com>
2021-03-04 02:02:17 +0000

    adding bootcamp_app files

---
+++ bootcamp_app/students.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'vagrant'
+});
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
